c0fe4c4b062ba8431ae78f7915e6793c676b0f28
Saeedafshari11235 <msafshari11235@gmail.com>
2021-09-25 15:57:46 +0330

    a new react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{ useState ,useRef ,useEffect} from "react";
+import TodoList from "./TodoList";
+import uuidv4 from "uuid/v4";
+
+const LOCAL_STORAGE_KEY = "todoapp.todos"
 
 function App() {
+  const [todos , setTodo] = useState([])
+  const todoRefname = useRef()
+
+  useEffect(()=>{
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos)setTodo(storedTodos)
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  },[todos])
+
+  function toggletodo(id){
+    const newtodos = [...todos]
+    const todo = newtodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodo(newtodos)
+  }
+
+  function handleAddtodo(e){
+    const name = todoRefname.current.value
+    if(name==="")return
+    setTodo(prevTodo =>{
+      return [...prevTodo , { id: uuidv4(), name: name, complete:false}]
+    })
+    todoRefname.current.value = null
+  }
+
+  function handleClearTodos(){
+    const newtodos = todos.filter(todo => !todo.complete)
+    setTodo(newtodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <TodoList todos={todos} toggletodo={toggletodo}/>
+    <input ref={todoRefname} type="text"/>
+    <button onClick={handleAddtodo}>Add Todo</button>
+    <button onClick={handleClearTodos}>Clear Completed</button>
+    <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
   );
 }
 
---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Todo({todo ,toggletodo}) {
+    function handleTodoClick(){
+        toggletodo(todo.id)
+    }
+
+    return (
+        <div>
+            <label>
+                <input type='checkbox' checked={todo.complete} onChange={handleTodoClick}/>
+                {todo.name}
+            </label>
+        </div>
+    )
+}
